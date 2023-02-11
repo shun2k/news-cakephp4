34fce2a343b2f44594e1707d5a1f3919be9302bd
shun2k <gerogeroshocker@gmail.com>
2023-02-11 14:22:47 +0900

    modified view/helper weatherDetail

--- html/src/View/Helper/ListupHelper.php
+++ html/src/View/Helper/ListupHelper.php
@@ -22,17 +22,14 @@ class ListupHelper extends Helper {
     'FukuokaXPrefecture'=>'39', 'Saga-ken'=>'40', 'NagasakiXPrefecture'=>'41', 'Kumamoto-ken'=>'42',
     'OitaXPrefecture'=>'43', 'Miyazaki-ken'=>'44', 'Kagoshima-ken'=>'45', 'Okinawa-ken'=>'46');
       
-  //------------- weatherNews --------------
-  // main 
+  //------------- weather main page --------------
+  // main page 
   public function getWeahterNews($data, $pref, $local, $pref_city) {
     $newsData = $data;
 
     $viewer = "";     // return data
     $printDate = [];  // 表示する日付
-    // $setTime = "";    // 天気リストの最初の時間を決める
-    // $timecheck = [];  // 表示する時間
-    // $printWeather = [];   // 表示する天気
-    // $printTemp = [];    // 表示する気温
+    
 
     // title -- 地名
     $areaname = $this->changeName($pref, $local, $pref_city);
@@ -42,106 +39,12 @@ class ListupHelper extends Helper {
     $nowWeahterTemp = $this->tempformat($newsData['oneday']['main']['temp']);
 
     // 基準時間の設定(現在の時刻)
-    // date_default_timezone_set('Asia/Tokyo');    // timezoneの設定
+  
     $nowDate = date('Y-m-d');
     array_push($printDate, $nowDate);
 
-    // 最初に表示する時間を決める（テーブルで表示する天気）
-    // もしforecastの最初のデータが次の日なら、当日の天気は空白にする
-    // for ($i=0; $i < 40; $i++) {
-    //   if ($nowDate <= substr($newsData['fivedays']['list'][$i]['dt_txt'], 0, 10)) {
-    //     $firstData = intval(substr($newsData['fivedays']['list'][$i]['dt_txt'], 11, 2));
-    //     print_r($firstData);
-    //     switch ($firstData) {
-    //       case '0':
-    //         $setTime = "03:00:00";
-    //         break;
-    //       case '3':
-    //         $setTime = "06:00:00";
-    //         break;
-    //       case '6':
-    //         $setTime = "09:00:00";
-    //         break;
-    //       case '9':
-    //         $setTime = "12:00:00";
-    //         break;
-    //       case '12':
-    //         $setTime = "15:00:00";
-    //         break;
-    //       case '15':
-    //         $setTime = "18:00:00";
-    //         break;
-    //       case '18':
-    //         $setTime = "21:00:00";
-    //         break;
-    //       default:
-    //         $setTime = "00:00:00";
-    //         $nowDate = date("Y-m-d", strtotime("+1 day"));   // nowDateを+1日する
-    //         break;
-    //     }
-    //     break;
-    //   }
-    // }
-
-    // 時間の設定
-    // $baseDateTime = new \DateTime($nowDate . $setTime);
-    // $baseDate = $baseDateTime->format('Y-m-d H:i:s');
-    // array_push($printDate, $this->formatdate($baseDate));
-    // print_r($baseDateTime);
-    // print_r($nowDate);
-    // $viewdata = "基準 - $baseDate \n";
-
-    // 配列に書くデータを格納 - 繰り返しを16から40に変更
-    // for ($i=0; $i < 24; $i++) {
-
-    //   if ($baseDate === $newsData['fivedays']['list'][$i]['dt_txt']) {
-    //     $icon = $this->changePicture($newsData['fivedays']['list'][$i]['weather'][0]['id']);
-    //     $setIcon = $this->Html->image($icon, ['class' => 'mainpageWeathericon']);
-    //     $setTemp = $this->tempformat($newsData['fivedays']['list'][$i]['main']['temp']);
-    //     array_push($printWeather, $setIcon);
-    //     array_push($printTemp, $setTemp);
-    //     $setTime = $this->formatTime($newsData['fivedays']['list'][$i]['dt_txt']);
-    //     array_push($timecheck, $setTime);
-
-    //     // $viewdata .= $baseDate . "\n";
-    //     $baseDateTime->modify('+3 hours');
-    //     $baseDate = $baseDateTime->format('Y-m-d H:i:s');
-
-    //     //printdate save
-    //     // if (substr($newsData['fivedays']['list'][$i]['dt_txt'], 11, 2) == '00') {
-    //     //   array_push($printDate, $this->formatdate($newsData['fivedays']['list'][$i]['dt_txt']));
-    //     // }
-    //   }
-
-    // }
    
-    // var_dump($timecheck);
-
-  //   $viewer .= '<p class = "category" id = "weather"><span>天気予報</span></p>' . "\n" .
-  //                          '<div class="col-sm-6">' . "\n" .
-  //                          ' <p class="text-center">'. $this->formatdate($nowDate) . $areaname . 'の天気</p>' . "\n" .
-  //                          ' <p class="text-center" >' . $this->Html->image($nowWeatherIcon, ['class' => 'nowWeather']) . '</p>' . "\n" .
-  //                          ' <p class="text-center">' . $nowWeahterTemp . ' ℃</p>' . "\n" .
-  //                          '</div>' . "\n" .
-  //                          '<div class="col-sm-6">' . "\n" .
-  //                          ' <table class="table">' . "\n" .
-  //                          '  <tbody class="table table-striped">' . "\n";
-
-
-  //         for ($i=0; $i < 4; $i++) {
-  //           $viewer .= '   <tr><td class="align-middle">' . $timecheck[$i] . '</td><td>' . $printWeather[$i] .
-  //           '</td><td class="align-middle">' . $printTemp[$i] . '℃</td></tr>' . "\n";
-  //         }
-
-  //         $viewer .= '  </tbody>' . "\n".
-  //                          ' </table>' . "\n".
-  //                          '</div>';
-  //         $link = $this->Html->link('もっと見る', ['controller' => 'News-users', 'action' => 'weatherDetail', $pref, $local]);
-  //         $viewer .= '<p>' . $link . '</p>' . "\n";
-  
-  //   return $viewer;
-  // }
-  $viewer .= '<p class = "category" id = "weather"><span>天気予報</span></p>' . "\n" .
+    $viewer .= '<p class = "category" id = "weather"><span>天気予報</span></p>' . "\n" .
                            '<div class="col-sm-6">' . "\n" .
                            ' <p class="text-center">'. $this->formatdate($nowDate) . $areaname . 'の天気</p>' . "\n" .
                            ' <p class="text-center" >' . $this->Html->image($nowWeatherIcon, ['class' => 'nowWeather']) . '</p>' . "\n" .
@@ -168,21 +71,21 @@ class ListupHelper extends Helper {
   
     return $viewer;
   }
+// -------- Weather main pgae 終わり ---------------
 
-// Weather
-//---- -----weatherDetail function------------------
+//---------weatherDetail function------------------
+// WeatherDetail page
   public function weatherDetail($data) {
-    $weatherData = $this->timeshift($data);     // データの日時を日本時間に修正
-    var_dump($weatherData);
-    date_default_timezone_set('Asia/Tokyo');    // timezoneの設定
+    $weatherData = $data;     // データの日時を日本時間に修正
+    // var_dump($weatherData);
+    // date_default_timezone_set('Asia/Tokyo');    // timezoneの設定
     $viewdata = "";   // returnするデータ
-    // $viewdata = $weatherData['oneday']['main']['temp'];
-
     $printDate = [];
     $printWeather = [];
     $printTemp = [];
     $timecheck = [];
-
+    $timeArray = ["03:00:00", "06:00:00", "09:00:00", "12:00:00", "15:00:00", "18:00:00", "21:00:00", "00:00:00"];
+   
     // 現在の情報をセット
     $icon = $this->changePicture($weatherData['oneday']['weather'][0]['id']);
     $setIcon = $this->Html->image($icon, ['class'=>'weatherpageIcons']);
@@ -190,110 +93,40 @@ class ListupHelper extends Helper {
     array_push($printTemp, $this->tempformat($weatherData['oneday']['main']['temp']));
     array_push($timecheck, "現在");
 
-    // 基準時間の設定
-    $nowDate = date('Y-m-d');
-    // array_push($printDate, $this->formatdate($nowDate));
-    $setTime = "";
-    // fivedayがcurrentより前日分から表記している場合があるので、時間合わせ
-    for ($i=0; $i < 40; $i++) {
-      if ($nowDate == substr($weatherData['fivedays']['list'][$i]['dt_txt'], 0, 10)) {
-        $firstData = intval(substr($weatherData['fivedays']['list'][$i]['dt_txt'], 11, 2));
-        switch ($firstData) {
-          case '0':
-            $setTime = "03:00:00";
-            break;
-          case '3':
-            $setTime = "06:00:00";
-            // array_push($printWeather, "-");
-            // array_push($printTemp, "-");
-            // array_push($timecheck, "-");
-            break;
-          case '6':
-            $setTime = "09:00:00";
+    
+    // 日付表示 -------------
+    $nowDate = new \DateTime(substr($weatherData['fivedays']['list'][0]['dt_txt'], 0, 10));
+    array_push($printDate, $this->formatdate($nowDate->format('Y-m-d')));
+    for ($i=1; $i<3; $i++) {
+      array_push($printDate, $this->formatdate($nowDate->modify("+1 days")->format('Y-m-d')));
+    }
+    
 
-            for ($i=0; $i < 1; $i++) {
-              array_push($printWeather, "-");
-              array_push($printTemp, "-");
-              array_push($timecheck, "-");
-            }
-            break;
-          case '9':
-            $setTime = "12:00:00";
-
-            for ($i=0; $i < 2; $i++) {
-              array_push($printWeather, "-");
-              array_push($printTemp, "-");
-              array_push($timecheck, "-");
-            }
-            break;
-          case '12':
-            $setTime = "15:00:00";
-
-            for ($i=0; $i < 3; $i++) {
-              array_push($printWeather, "-");
-              array_push($printTemp, "-");
-              array_push($timecheck, "-");
-            }
-            break;
-          case '15':
-            $setTime = "18:00:00";
-
-            for ($i=0; $i < 4; $i++) {
-              array_push($printWeather, "-");
-              array_push($printTemp, "-");
-              array_push($timecheck, "-");
-            }
-            break;
-          case '18':
-            $setTime = "21:00:00";
-
-            for ($i=0; $i < 5; $i++) {
-              array_push($printWeather, "-");
-              array_push($printTemp, "-");
-              array_push($timecheck, "-");
-            }
-            break;
-          case '21':
-              // 翌日の設定
-              $setTime = "00:00:00";
-              $nowDate = date("Y-m-d", strtotime("+1 day"));
-          default:
-            break;
-        }
+    
+// timeArrayの時刻とweatherDataの時刻を比較し、weatherData最初の時間までの各項目を "-"とする。
+    for ($i=0; $i < 8; $i++) {
+      if ($timeArray[$i] !== substr($weatherData['fivedays']['list'][0]['dt_txt'], 11)) {
+        array_push($timecheck, "-");
+        array_push($printWeather, "-");
+        array_push($printTemp, "-");
+      } else {
         break;
-
       }
     }
 
-    // 時間の設定
-    $baseDateTime = new \DateTime($nowDate . $setTime);
-    $baseDate = $baseDateTime->format('Y-m-d H:i:s');
-    array_push($printDate, $this->formatdate($baseDate));
+  // 各データの配列に表示データを入れていく
+  for ($i=0; $i < 24; $i++) {
+    // 時間
+    array_push($timecheck, $this->formatTime($weatherData['fivedays']['list'][$i]['dt_txt']));
+    // 天気のアイコン
+    $icon = $this->changePicture($weatherData['fivedays']['list'][$i]['weather'][0]['id']);
+    $setIcon = $this->Html->image($icon,['class'=>'weatherpageIcons']);
+    array_push($printWeather, $setIcon);
+    // 気温
+    array_push($printTemp, $this->tempformat($weatherData['fivedays']['list'][$i]['main']['temp']));
+  }
+    
 
-
-    // $viewdata = "基準 - $baseDate \n";
-    for ($i=0; $i < 40; $i++) {
-
-      if ($baseDate === $weatherData['fivedays']['list'][$i]['dt_txt']) {
-        $icon = $this->changePicture($weatherData['fivedays']['list'][$i]['weather'][0]['id']);
-        $setIcon = $this->Html->image($icon,['class'=>'weatherpageIcons']);
-        $setTemp = $this->tempformat($weatherData['fivedays']['list'][$i]['main']['temp']);
-        array_push($printWeather, $setIcon);
-        array_push($printTemp, $setTemp);
-        $setTime = $this->formatTime($weatherData['fivedays']['list'][$i]['dt_txt']);
-        array_push($timecheck, $setTime);
-
-        // $viewdata .= $baseDate . "\n";
-        $baseDateTime->modify('+3 hours');
-        $baseDate = $baseDateTime->format('Y-m-d H:i:s');
-
-        //printdate save
-        if (substr($weatherData['fivedays']['list'][$i]['dt_txt'], 11, 2) == '00') {
-          array_push($printDate, $this->formatdate($weatherData['fivedays']['list'][$i]['dt_txt']));
-        }
-      }
-
-    }
 
     //表示するためのデータ
     $k = 0;
@@ -350,6 +183,8 @@ class ListupHelper extends Helper {
     return $viewdata;
 
   }
+  // ------ 終わり weatherdetail -----------------
+
 
   // id→picture 変換
   public function changePicture($id) {
@@ -446,25 +281,6 @@ class ListupHelper extends Helper {
     $returnData = $time . " 時";
     return $returnData;
   }
-
-  // // ニュース名日本語表示
-  // public function changeCategories($cat) {
-  //   return $this->catNames[$cat];
-  // }
-
-  // forecastの日時データを修正
-  // public function timeshift($data) {
-  //   $returnData = $data;
-  //   for ($i=0; $i < 40; $i++) {
-  //     $choiceDate = new \Datetime($returnData['fivedays']['list'][$i]['dt_txt']);
-  //     $choiceDate->modify('+9 hours');
-  //     $formatDate = $choiceDate->format('Y-m-d H:i:s');
-  //     $returnData['fivedays']['list'][$i]['dt_txt'] = $formatDate;
-  //   }
-  //   return $returnData;
-  // }
-
-  // ----- weather_detail ----------
 
   // 都道府県名表示 -- title
   public function changeName($pref, $local, $data) {
